f85dcfce56c0118720877b827b9857a8a5b1314a
gabriel-olv <br.gabrieldeoliveira@gmail.com>
2023-03-09 18:26:51 -0300

    Cpf/Cnpj validation

---
+++ src/App.java
@@ -0,0 +1,6 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        // System.out.println(Validation.CPF.isValid("536.251.690-61"));
+        // System.out.println(Validation.CNPJ.isValid("11.108.311/0001-95"));
+    }
+}
